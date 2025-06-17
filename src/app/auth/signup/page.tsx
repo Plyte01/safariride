378a1b51aeabb056b406c764cac3aa3b8607023f
Plyte01 <plantonmakana59@gmail.com>
2025-06-17 12:18:10 +0300

    Update page.tsx on line 147 fo rphone number optional

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -144,7 +144,7 @@ export default function SignupPage() {
               <PhoneInput
                 id="phone"
                 name="phone"
-                required //to make phone number optional,remove this
+                //required //to make phone number optional,remove this
                 className="mt-1 appearance-none block w-full px-3 py-2 pl-10 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                 placeholder="+254712345678"
                 value={phoneNumber}
